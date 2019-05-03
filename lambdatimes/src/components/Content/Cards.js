26ecf08f3d925ecbfb547fea766a48e52ae8db71
roman-jordan <rjordan1352@gmail.com>
2019-05-03 09:11:53 -0700

    MVP Complete

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import {cardData} from '../../data';
 import Card from './Card';
 
-const Cards = props => {
-  return (
-    <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
-    </div>
-  )
+export default class Cards extends Component{
+  state = {
+    cards:[]
+  }
+  componentDidMount(){
+    this.setState({
+      cards: cardData
+    })
+  }
+
+  render(){
+    return (
+      <div className="cards-container">
+        {this.state.cards.map((card, i)=>
+          <Card key={i} card={card} />
+        )}
+      </div>
+    )
+  }
 }
 
 // Make sure you include prop types for all of your incoming props
-
-export default Cards;
